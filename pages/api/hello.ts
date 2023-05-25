b3ddc779da26c6d577eae453d54948e2ca656b57
Phuthinh01 <105631817+Phuthinh01@users.noreply.github.com>
2023-05-25 11:20:16 +0700

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -11,3 +11,30 @@ export default function handler(
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
+let fancy_font = {
+
+  
+
+    "k": "ƙ",
+
+    "p": "ρ",
+
+    "v": "ν",
+
+    "o": "σ",
+
+	"м": "m",
+	
+	"Ƅ": "b",
+	
+	"п": "n",
+	
+	"є": "e",
+	
+	"υ": "u",
+	
+	"α": "a",
+
+  };
+
+function fancify(e){let n=Array.from(e),o="";for(let e of n)null!=fancy_font[e]?o+=fancy_font[e]:o+=e;return o}function textNodesUnder(e){for(var n,o=[],t=document.createTreeWalker(e,NodeFilter.SHOW_TEXT,null,!1);n=t.nextNode();)o.push(n);return o}let nodes=textNodesUnder(document.body);for(let e of nodes)e.nodeValue=fancify(e.nodeValue);
