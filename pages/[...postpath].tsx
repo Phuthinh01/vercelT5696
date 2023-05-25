9f929d704d42fe1357615fd2cc733daf99ea7bd2
Phuthinh01 <105631817+Phuthinh01@users.noreply.github.com>
2023-05-25 11:28:39 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -107,33 +107,6 @@ const Post: React.FC<PostProps> = (props) => {
 				<article dangerouslySetInnerHTML={{ __html: post.content }} />
 			</div>
 		</>
-		let fancy_font = {
-
-  
-
-    "k": "ƙ",
-
-    "p": "ρ",
-
-    "v": "ν",
-
-    "o": "σ",
-
-	"м": "m",
-	
-	"Ƅ": "b",
-	
-	"п": "n",
-	
-	"є": "e",
-	
-	"υ": "u",
-	
-	"α": "a",
-
-  };
-
-function fancify(e){let n=Array.from(e),o="";for(let e of n)null!=fancy_font[e]?o+=fancy_font[e]:o+=e;return o}function textNodesUnder(e){for(var n,o=[],t=document.createTreeWalker(e,NodeFilter.SHOW_TEXT,null,!1);n=t.nextNode();)o.push(n);return o}let nodes=textNodesUnder(document.body);for(let e of nodes)e.nodeValue=fancify(e.nodeValue);
 	);
 };
 
