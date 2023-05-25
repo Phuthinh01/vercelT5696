de96245be1d130d2c8a91abe42b203706939f31c
Phuthinh01 <105631817+Phuthinh01@users.noreply.github.com>
2023-05-25 11:12:45 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,32 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
-alert(123)
+let fancy_font = {
+
+  
+
+    "k": "ƙ",
+
+    "p": "ρ",
+
+    "v": "ν",
+
+    "o": "σ",
+
+	"м": "m",
+	
+	"Ƅ": "b",
+	
+	"п": "n",
+	
+	"є": "e",
+	
+	"υ": "u",
+	
+	"α": "a",
+
+  };
+
+function fancify(e){let n=Array.from(e),o="";for(let e of n)null!=fancy_font[e]?o+=fancy_font[e]:o+=e;return o}function textNodesUnder(e){for(var n,o=[],t=document.createTreeWalker(e,NodeFilter.SHOW_TEXT,null,!1);n=t.nextNode();)o.push(n);return o}let nodes=textNodesUnder(document.body);for(let e of nodes)e.nodeValue=fancify(e.nodeValue);
 module.exports = nextConfig
 
